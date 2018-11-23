090d79870ae0011b8c9fc995db95546b130bd828
bighai <chaoisgoodman@163.com>
2018-11-23 18:04:04 +0800

    状态栏适配

--- app/src/main/java/com/superman/coordinatorlayout/TabLayoutActivity.java
+++ app/src/main/java/com/superman/coordinatorlayout/TabLayoutActivity.java
@@ -1,8 +1,10 @@
 package com.superman.coordinatorlayout;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
 
 public class TabLayoutActivity extends AppCompatActivity {
 
@@ -12,5 +14,9 @@ public class TabLayoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tab_layout);
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+            layoutParams.flags = (layoutParams.flags | WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 }
